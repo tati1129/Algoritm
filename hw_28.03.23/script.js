e8c2b3c3391c1f123de98282314dee933d4a40e3
tati1129 <tatka.bogat@gmail.com>
2023-03-28 17:07:44 +0200

    2commit

--- hw_28.03.23/script.js
+++ hw_28.03.23/script.js
@@ -22,22 +22,30 @@ function randomLift(a,b){ //ф-ция выбора рандомного лифт
 function elevator(a, b, stage){
     let distanceA = 0;
     let distanceB = 0;
-    
-    if(a !== stage || b !== stage){
+    if( a===b && a === stage){
+        return `Elevators on your floor`
+    }else if (a !== b && a === stage) {
+        return  `Elevator A on your floor`;
+    }else if (a !== b && b === stage) {
+        return  `Elevator B on your floor`;
+    }else if(a !== b !== stage){
         distanceA = Math.abs(stage - a);
         distanceB = Math.abs(stage - b);
         if (distanceA === distanceB){ //если оба лифта на одном этаже, выбираем рандомный
             let a = 'Elevator A on the way ';
             let b = 'Elevator B on the way ';
             return  `${randomLift(a, b)} in ${distanceA} stage`
-        }
-        if(distanceA <= distanceB){
+        }else if(distanceA < distanceB){
            return  `Elevator A on the way in:  ${distanceA} stage`
-            
         } else if(distanceA > distanceB){
             return  `Elevator B on the way in:  ${distanceB}  stage` 
         } 
     }
+    return `Elevators on your floor`
 }
 
-console.log(elevator(0,8,1)); 
+console.log(elevator(0,8,1));
+console.log(elevator(8,0,1));
+console.log(elevator(0,0,0));
+console.log(elevator(2,2,1));
+console.log(elevator(8,2,6));
